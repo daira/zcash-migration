c3506fe2491f692ee2df7826a09d5f3f2b5be039
Daira Hopwood <daira@jacaranda.org>
2018-12-17 16:34:31 +0000

    This code simulates the proposed strategy for migrating Zcash Sprout funds to Sapling, as described in draft ZIP 308.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

---
+++ README.md
@@ -0,0 +1,5 @@
+Sprout -> Sapling migration simulation
+--------------------------------------
+
+This code simulates the proposed strategy for migrating Zcash Sprout
+funds to Sapling, as described in draft ZIP 308. [MIT licensed](LICENSE).
---
+++ migration.py
@@ -0,0 +1,51 @@
+import random
+from collections import deque
+
+INTERVAL_BLOCKS = 500
+LATENCY_BLOCKS = 10
+DUST_ZATOSHI = 1000000
+(MIN_EXPONENT, MAX_EXPONENT) = (6, 9)
+(MIN_MANTISSA, MAX_MANTISSA) = (1, 99)
+TXNS_PER_BATCH = 5
+
+ZEC = 100000000
+
+
+def div_ceil(x, y):
+    return (x+y-1)//y
+
+def amount_to_send(unmigrated_amount):
+    while True:
+        exponent = random.randint(MIN_EXPONENT, MAX_EXPONENT)
+        amount = 10**exponent * random.randint(MIN_MANTISSA, MAX_MANTISSA)
+        if amount <= unmigrated_amount:
+            return amount
+
+def simulate(unmigrated_amount):
+    amounts = deque()
+    while unmigrated_amount > DUST_ZATOSHI:
+        amount = amount_to_send(unmigrated_amount)
+        amounts.append(amount)
+        unmigrated_amount -= amount
+
+    #print list(amounts)
+    #print len(amounts)
+    return amounts
+
+def table(total_amount):
+    times = deque()
+    n = 100000
+    for i in xrange(n):
+        amounts = simulate(total_amount)
+        latency = random.randint(LATENCY_BLOCKS, LATENCY_BLOCKS+INTERVAL_BLOCKS)
+        times.append((div_ceil(len(amounts), TXNS_PER_BATCH)*INTERVAL_BLOCKS + latency)/576.0)
+
+    times = list(times)
+    times.sort()
+    print "%5.2f %5.2f %5.2f" % (times[n/10], times[n/2], times[n*9/10])
+
+table(1*ZEC)
+table(10*ZEC)
+table(100*ZEC)
+table(1000*ZEC)
+table(10000*ZEC)
